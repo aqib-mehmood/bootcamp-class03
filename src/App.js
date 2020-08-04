092ce8348e18f2e6a3dc49d4156d19d61ccd49c5
Aqib <aqimehmoodb@gmail.com>
2020-08-04 23:40:48 +0500

    modifying the files

--- src/App.js
+++ src/App.js
@@ -2,25 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/* *This is also the way to call the properties.
+function App({name,age}) {
+return <h1>
+        Hello World using AppJs by {name} Age = {age}
+      </h1>
+}*/
+
+function App(props) {
+  return <div>
+          Hello World using AppJs by {props.name} Age = {props.age}
+          <div>This is a child div</div>
+        </div>
+  }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App name="aqib" age={20}/>,
   document.getElementById('root')
 );
 
