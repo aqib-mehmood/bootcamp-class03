5b4c54fe97c237949bbf7f8449c46957e95fc0f1
Aqib <aqimehmoodb@gmail.com>
2020-08-06 14:38:39 +0500

    Added Room component for assignment

--- src/Room.js
+++ src/Room.js
@@ -1,30 +1,52 @@
 import React, {useState} from 'react';
+import './custom.css';
 
 
 function Room(params) {
 
     let [isLit, setLit] = useState(true); //in  useState we assign the initial value of state.
-    let [isAge, setAge] = useState(20);
+    let [isTemp, setTemp] = useState(22);
 
-    function UpdateLit() {
-        setLit(!isLit);
+    function onLit() {
+        if (isLit == false) {
+            setLit(!isLit);
+        }
+        else{
+            alert("Light is already ON");
+        }
     }
 
-    function UpdateAge(){
-        setAge(++isAge);
+    function offLit() {
+        if (isLit == true) {
+            setLit(!isLit);
+        }
+        else{
+            alert("Light is already OFF");
+        }
+    }
+
+
+    function increaseTemp(){
+        setTemp(++isTemp);
+    }
+
+    function descreaseTemp(){
+        setTemp(--isTemp);
     }
 
    // const UpdateAge = () => setAge(++isAge)
+   //{`room ${isLit ? 'Lit' : 'dark'}`}
 
     return(
-        <div>
-            This Room is {isLit ? 'Lit' : 'dark'}
+        <div className={"room "+ (isLit ? 'Lit' : 'dark')}> 
+            Light Status = {isLit ? 'ON' : 'OFF'}
             <br/>
-            <button onClick={UpdateLit}>Toggle Lit</button>
-            <br/>
-            Age = {isAge}
-            <br/>
-            <button onClick={UpdateAge}>Increase Age</button>
+            Temperature = {isTemp} Deg Celc
+            <br/><br/>
+            <button onClick={onLit}>Lit ON</button>
+            <button onClick={offLit}>Lit OFF</button>
+            <button onClick={increaseTemp}>Temp + </button>
+            <button onClick={descreaseTemp}>Temp - </button>
         </div>
     )
 }
